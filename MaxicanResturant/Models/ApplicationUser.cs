31e9db4ca230baaf5322cd87b6cf22fe49351003
mdazmainabrar <abrarmain.aa@gmail.com>
2026-04-26 14:29:56 +0600

    ICollection

--- MaxicanResturant/Models/ApplicationUser.cs
+++ MaxicanResturant/Models/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace MaxicanResturant.Models
 {
     public class ApplicationUser : IdentityUser
     {
-       public ICollection<Order>? Orders { get; set; }
+       public ICollection<Order>? Orders { get; set; } //if we want to get all the orders of a user we can use this property to do that. this is a navigation property that allows us to access the orders associated with a user. it is defined as a collection of Order objects, and it is nullable (indicated by the ?), meaning that a user may not have any orders. this property can be used in our application to retrieve and manage the orders placed by a specific user.
     }
 }
