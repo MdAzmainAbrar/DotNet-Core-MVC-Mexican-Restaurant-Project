f99a253f73ea39cc3e29da27cd376ccd9a4959c0
mdazmainabrar <abrarmain.aa@gmail.com>
2026-04-26 14:32:43 +0600

    ShowRequestId

--- MaxicanResturant/Models/ErrorViewModel.cs
+++ MaxicanResturant/Models/ErrorViewModel.cs
@@ -4,6 +4,13 @@ namespace MaxicanResturant.Models
     {
         public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId); //this is a read-only property that returns a boolean
+                                                                       //value indicating whether the RequestId property has
+                                                                       //a value or not. it uses the string.IsNullOrEmpty
+                                                                       //method to check if the RequestId is null or an empty
+                                                                       //string. if the RequestId has a value, ShowRequestId
+                                                                       //will return true, otherwise it will return false. this
+                                                                       //property can be used in the view to conditionally display
+                                                                       //the RequestId if it is available. 
     }
 }
